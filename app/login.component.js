63fd874b6c1883bfa31612e3bb365910cb136b5d
Cameron Kidman <cameronjkidman@gmail.com>
2016-01-13 19:19:03 -0700

    Little farther...

--- app/login.component.js
+++ app/login.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', './services/api.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,52 +8,31 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, api_service_1;
     var LoginComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (api_service_1_1) {
+                api_service_1 = api_service_1_1;
             }],
         execute: function() {
             LoginComponent = (function () {
-                function LoginComponent() {
-                    this.tokenName = getCwTokenName();
-                    this.tokenGetter = getCwAuthToken();
-                    this.jwtHelper = new JwtHelper();
-                    headerName = "application/vnd.calendarworkouts.v1";
-                    headerPrevis = "Accept";
+                function LoginComponent(apiService) {
+                    this.apiService = apiService;
                 }
-                LoginComponent.prototype.authHeaderName = function () {
-                    this.headerName;
-                };
-                LoginComponent.prototype.authHeaderPrefix = function () {
-                    this.headerPrefix;
-                };
-                //userFetch() {
-                //    this._userService.getUserDashboard().subscribe(res => this.user = res);
-                //}
-                LoginComponent.prototype.getCwAuthToken = function () {
-                    return this.jwtHelper.localStorage.getItem('access-token');
-                };
-                LoginComponent.prototype.getCwTokenName = function () {
-                    return this.jwtHelper.localStorage.getItem('client');
-                };
-                LoginComponent.prototype.loggedIn = function () {
-                    return tokenNotExpired();
-                };
-                LoginComponent.prototype.logout = function () {
-                };
-                LoginComponent.prototype.login = function () {
-                };
                 LoginComponent = __decorate([
                     core_1.Component({
                         selector: 'login-links',
-                        template: "\n    <div *ngIf=\"!loggedIn()\">\n        <button (click)=\"login()\">Login</button>\n        <span>Or...</span>\n        <div>\n\n        </div>\n    </div>\n    <button *ngIf=\"loggedIn()\" (click)=\"logout()\">Logout</button>\n    "
+                        template: "\n    <div *ngIf=\"!loggedIn()\">\n        <button (click)=\"apiService.login()\">Login</button>\n        <span>Or...</span>\n        <div>\n\n        </div>\n    </div>\n    <button *ngIf=\"loggedIn()\" (click)=\"logout()\">Logout</button>\n    ",
+                        bindings: [api_service_1.ApiService],
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [(typeof (_a = typeof api_service_1.ApiService !== 'undefined' && api_service_1.ApiService) === 'function' && _a) || Object])
                 ], LoginComponent);
                 return LoginComponent;
+                var _a;
             })();
             exports_1("LoginComponent", LoginComponent);
         }
--- app/login.component.ts
+++ app/login.component.ts
@@ -1,64 +1,28 @@
 import {Component} from 'angular2/core';
 import {Http} from 'angular2/http';
 import {AuthHttp, AuthConfig} from 'angular2-jwt';
+import {ApiService} from './services/api.service';
 
 
 @Component({
     selector: 'login-links',
     template: `
     <div *ngIf="!loggedIn()">
-        <button (click)="login()">Login</button>
+        <button (click)="apiService.login()">Login</button>
         <span>Or...</span>
         <div>
 
         </div>
     </div>
     <button *ngIf="loggedIn()" (click)="logout()">Logout</button>
-    `
+    `,
+    bindings: [ApiService],
 
 })
 
 export class LoginComponent {
-    public headerName: string;
-    public headerPrefix: string;
-    public tokenName = getCwTokenName();
-    public tokenGetter = getCwAuthToken();
-    private jwtHelper = new JwtHelper();
 
-    constructor() {
-        headerName = "application/vnd.calendarworkouts.v1";
-        headerPrevis = "Accept";
-    }
-
-    authHeaderName() {
-        this.headerName;
-    }
-
-    authHeaderPrefix() {
-        this.headerPrefix;
-    }
-
-    //userFetch() {
-    //    this._userService.getUserDashboard().subscribe(res => this.user = res);
-    //}
-
-    getCwAuthToken(): string {
-        return this.jwtHelper.localStorage.getItem('access-token');
-    }
-
-    getCwTokenName(): string {
-        return this.jwtHelper.localStorage.getItem('client');
-    }
-
-    loggedIn() {
-        return tokenNotExpired();
-    }
-
-    logout() {
-
-    }
-
-    login() {
+    constructor(public apiService: ApiService) {
 
     }
 }
